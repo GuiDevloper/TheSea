7bb0983b7f3282e863084f3d0edeae5602034d99
GuiDevloper <guicorrea384@gmail.com>
2018-08-23 20:33:12 -0300

    Corrected getters of DOM

--- JS/sea.js
+++ JS/sea.js
@@ -24,7 +24,6 @@ function calculateSVG() {
 		"; stroke-dashoffset: " + pathLength +
 		"; animation: draw 4s forwards;";
 	setAnimation([...paths, ...circles], strokeAndAnim);
-	console.log(pathLength);
 	c1 = draw.querySelectorAll("path, circle");
 }
 calculateSVG();
@@ -54,8 +53,6 @@ raias[0].appendChild(raia_r);
 var fish_index = 0;
 for (var elem of [...raias].values()) {
 	elem.innerHTML = raias[0].innerHTML;
-	var $tudo = elem.parentElement;
-	var deep = $tudo.classList.value.replace('tudo t', '');
 	for (var raia of [...elem.children].values()) {
 		//Abre o node e percorre adding events de click
 		raia.addEventListener('click', function(e) {
@@ -70,7 +67,7 @@ for (var elem of [...raias].values()) {
 				tran = "translateX(-5px) rotate(180deg)";
 				fish_index = fish_index == 5 ? 0 : fish_index+1;
 			}
-			getFish("./Fishs/Descriptions/Deep" + deep + ".json?q=test&amp;rnd=" + Math.random(), fish_index, $tudo);
+			getFish(fish_index, $this.parentElement.parentElement);
 			//Mudando atual para translateX(0)
 			setAnimation([$this], $this.style.transform.replace(
 				tran.replace(" rotate(180deg)", ""), "translateX(0px)"
@@ -93,7 +90,9 @@ var Ajax = {
 		return $this.readyState == 4 && $this.status == 200;
 	}
 };
-function getFish(url, index, $tudo) {
+function getFish(index, $tudo) {
+	var deep = $tudo.classList.value.replace('tudo t', '');
+	var url = "./Fishs/Descriptions/Deep" + deep + ".json?q=test&amp;rnd=" + Math.random();
 	Ajax.send(url, "GET");
 	xhr.onreadystatechange = function() {
 		if(Ajax.isReady(this)) {
@@ -101,8 +100,8 @@ function getFish(url, index, $tudo) {
 	  	var fish = JSON.parse(data)[index];
 	  	$tudo.getElementsByClassName("nome1")[0].innerHTML = fish[0];
 	  	$tudo.getElementsByClassName("nome2")[0].innerHTML = fish[1];
-	  	$tudo.getElementsByClassName("desc").innerHTML = fish[2];
-	  	$tudo.getElementsByClassName("animal").innerHTML = fish[3];
+	  	$tudo.getElementsByClassName("desc")[0].innerHTML = fish[2];
+	  	$tudo.getElementsByClassName("animal")[0].innerHTML = fish[3];
 	  	calculateSVG();
 		}
 	};
